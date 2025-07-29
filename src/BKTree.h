dfa91f538732cb925a97aad68479fd6cd66a02b1
Gatito7218 <tegodhd@gmail.com>
2025-07-28 20:58:30 -0400

    Comments on the tree

--- src/BKTree.h
+++ src/BKTree.h
@@ -10,9 +10,11 @@
 using namespace std;
 
 //TO-DO: multiple children of the same distance, will use a vector, I don't wanna do that right now tho
+//Also I did not account for the word rank yet, just wanted to do the basic code logic of the tree
 struct Node {
     string word;
     unordered_map<int, unique_ptr<Node>> children;
+    int rank; //the file used to train has the frequency of each english language word ranked
     
     Node(string& w) : word(w) {};
     void addChildren(string& inpW, int distance);
